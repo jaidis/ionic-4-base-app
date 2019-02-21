ea14c83d3a87084a2ac83176a177786a9ff9b6ea
Manuel Muñoz <heidiaricel@hotmail.com>
2019-02-21 12:38:20 +0100

    Editadas las vistas de las pestañas
    Se ha editado el contenido de la pestañas

--- src/app/pages/tab3/tab3.page.ts
+++ src/app/pages/tab3/tab3.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab3',
@@ -7,7 +8,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Tab3Page implements OnInit {
 
-  constructor() { }
+  constructor(public toastController: ToastController) { }
+
+  async presentToast() {
+    const toast = await this.toastController.create({
+      message: 'Acción confirmada correctamente.',
+      duration: 2000
+    });
+    toast.present();
+  }
+
+  async presentToast2() {
+    const toast = await this.toastController.create({
+      message: 'Acción confirmada correctamente.',
+      showCloseButton: true,
+      position: 'top',
+      closeButtonText: 'Cerrar'
+    });
+    toast.present();
+  }
+
+  async presentToast3() {
+    const toast = await this.toastController.create({
+      animated: true,
+      color: 'danger',
+      message: '¡Porqué tocas!',
+      position: 'middle',
+      translucent: true,
+      duration: 2000
+    });
+    toast.present();
+  }
 
   ngOnInit() {
   }
